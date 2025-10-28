eabbafa1ba2f2feff9502e6a0d300196cef8cb0a
wilki-BD <wilkimitrixiaa@gmail.com>
2025-10-27 20:33:24 -0600

    subir base

---
+++ base/platforms/android/app/src/main/assets/www/script.js
@@ -0,0 +1,24 @@
+let b = document.getElementById('b');
+let r = document.getElementById('r');
+let d = document.getElementById('d');
+let body = document.body ;
+
+
+document.addEventListener('deviceready', () => {
+
+    b.addEventListener('click', () => {
+
+        let nombre = d.value;
+
+      let db = sqlitePlugin.openDatabase({name: 'base23.db', location:'default'});
+
+      db.transaction((tx) => {
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS datos (nombres TEXT);');
+        tx.executeSql('INSERT INTO datos (nombres) VALUES (?);',[nombre]);
+
+      },() => body.style.background = 'red',body.style.background = 'green');
+
+    });
+
+});
